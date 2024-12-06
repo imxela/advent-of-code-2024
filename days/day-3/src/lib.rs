e23e443fcd76a3906976d41717125f946c1b2a3a
imxela <65206220+imxela@users.noreply.github.com>
2024-12-06 10:45:10 +0100

    wip: add day 3 boilerplate

--- days/day-3/src/lib.rs
+++ days/day-3/src/lib.rs
@@ -1,5 +1,26 @@
-pub fn day_3_part_1(input: &str) -> usize {
-    todo!()
+#![allow(clippy::cargo_common_metadata)]
+
+use common::AdventSolver;
+
+struct Day3Solver {
+    data: Vec<u8>,
+}
+
+impl AdventSolver<usize, Vec<u8>> for Day3Solver {
+    fn parse(input: &str) -> Self {
+        Day3Solver {
+            data: input.as_bytes().to_vec(),
+        }
+    }
+
+    fn solve(&self) -> common::AdventSolution<usize> {
+        dbg!(&self.data);
+        161.into()
+    }
+
+    fn data(&self) -> &Vec<u8> {
+        &self.data
+    }
 }
 
 #[cfg(test)]
@@ -7,9 +28,11 @@ mod tests {
     use super::*;
 
     #[test]
-    fn day_3_part_1_example() {
-        let input = common::read_example_input(1);
-
-        assert!(day_3_part_1(&input) == 161);
+    fn test_day_3_part_1() {
+        let input = String::from("3"); // common::read_example_input(3);
+        let solver = Day3Solver::parse(&input);
+        let solution = solver.solve();
+        println!("Solution is {solution}");
+        assert!(solution.prove(&161));
     }
 }
